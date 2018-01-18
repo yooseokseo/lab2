9115966eec5e5ec3387dbb4d2bf2e379fd45ca43
yooseok Seo <yos017@ucsd.edu>
2018-01-18 01:08:43 -0800

    added Javscript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,17 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("a.thumbnail").click(projectClick);
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) { 
+    // prevent the page from reloading 
+    e.preventDefault();
+    // In an event handler, $(this) refers to 
+    // the object that triggered the event 
+    $(this).css("background-color", "#7fff00");
 }
